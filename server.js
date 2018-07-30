dfd309e296e2ba9e7a31a4d14e57905bed1fd9dc
Wade Aston <mrwadepro@gmail.com>
2018-07-29 22:43:37 -0500

    method-override added into client and server

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/videos", videos);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   //set static foler
-  app.user(express.static("client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
