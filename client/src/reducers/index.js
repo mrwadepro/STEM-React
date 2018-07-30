411334e3b8401ea493c1de0d86550ed024e98059
Unknown <mrwadepro@gmail.com>
2018-07-29 22:28:38 -0500

    After Prod fix...

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "../../../../../Library/Caches/typescript/2.9/node_modules/redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
